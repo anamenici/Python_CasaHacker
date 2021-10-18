bfbc37fd2c19e7a7f13edd8f8f86447a1a9ef96a
Ana Menici <anamenici@gmail.com>
2021-10-18 16:41:56 -0300

    Aula 1

---
+++ nome.py
@@ -0,0 +1,9 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+nome = input("Qual é o seu nome? ")
+Qual é o seu nome? Ana
+print("Olá, ", nome)
+Olá,  Ana
+print("Olá,", nome)
+Olá, Ana
+Olá, Ana
---
+++ numero.py
@@ -0,0 +1,4 @@
+tempo = int(input("Escreva o número de minutos: "))
+horas = tempo // 60
+minutos = tempo % 60
+print(f"Equivale a {horas}hora(s) e {minutos}minutos(s)")
